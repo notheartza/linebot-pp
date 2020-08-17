5e9f64a14ab2e8772d49a5d69c2fd990ab0291ce
notheartza <not.102@hotmail.com>
2020-08-17 09:51:12 +0700

    Update app.py

--- linebot-pp/app.py
+++ linebot-pp/app.py
@@ -472,10 +472,10 @@ def handle_postback(event):
     #return render_template('Broadcast.html' , typeText=type)
 
 
-@app.route('/Broadcast/<gettype>' ,methods=['GET'])
+@app.route('/Broadcast/<gettype>' , methods=['GET'])
 def Broadcast_type(gettype):
     #line_bot_api.broadcast(TextSendMessage(text='นักเรียนคนไหนยังไม่ได้รับสมุดให้มารับสมุดที่ห้องพักครูคอมพิวเตอร์นะครับ'))
-
+    type = ""
     choices = [{"รายบุคคล": '1'},{"มากว่า 1 คนขึ้นไป": '2'},{"ทั้้งหมด":'3'}]
     for i in choices:
         if i.values == gettype:
