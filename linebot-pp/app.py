1546ea14eb5571fa1fc469b9a9b5af47be10d902
notheartza <not.102@hotmail.com>
2020-08-17 14:52:28 +0700

    hey

--- linebot-pp/app.py
+++ linebot-pp/app.py
@@ -480,11 +480,18 @@ def Broadcast_type(gettype):
     #line_bot_api.broadcast(TextSendMessage(text='นักเรียนคนไหนยังไม่ได้รับสมุดให้มารับสมุดที่ห้องพักครูคอมพิวเตอร์นะครับ'))
     type = ""
     choices = {'รายบุคคล': '1', 'มากว่า 1 คนขึ้นไป': '2', 'ทั้งหมด':'3'}
+    
+    usersheet = clientgs('usersheet', client)        
+    users = logsheet.get_all_records()
+    linename = []
+    for i in users:
+        linename.append(i["userName"])
+
     for key, value in choices.items():
         if value == gettype:
             type = key
 
-    return render_template('Broadcast.html' , typeText=type)
+    return render_template('Broadcast.html' , typeText=type, userline=linename)
 
 
 
