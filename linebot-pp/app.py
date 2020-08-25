cda43e7a90eb446668eee61d6944c4f850971354
notheartza <not.102@hotmail.com>
2020-08-25 15:25:04 +0700

    หกหฟ

--- linebot-pp/app.py
+++ linebot-pp/app.py
@@ -18,10 +18,12 @@ import emoji
 from .exsheet import client
 from gspread.models import Cell
 from flask_httpauth import HTTPBasicAuth
-from firebase_admin import auth
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 
-cred = credentials.Certificate("./linebot-pp-firebase-adminsdk-pkt20-eb15ce9f27.json")
+cred = ccredentials.RefreshToken("./linebot-pp-firebase-adminsdk-pkt20-eb15ce9f27.json")
 default_app = firebase_admin.initialize_app(cred)
 app = Flask(__name__) #top-----------------
 print(default_app.name) 
