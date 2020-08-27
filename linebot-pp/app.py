d9b66763c5415fffc982bb813b8a486a51ce1d09
notheartza <not.102@hotmail.com>
2020-08-27 09:40:25 +0700

    da

--- linebot-pp/app.py
+++ linebot-pp/app.py
@@ -25,7 +25,7 @@ import pyrebase
 
 config = {
   "apiKey": "AIzaSyCO3DgD2x6fJuageulBP9i1l619Ee54beA",
-  "authDomain": "linebot-pp.firebaseapp.com",
+  "authDomain": "https://linebot-pp.firebaseapp.com",
   "databaseURL": "https://linebot-pp.firebaseio.com/",
   "storageBucket": "linebot-pp.appspot.com",
   "serviceAccount": "./linebot-pp/linebot-pp-firebase-adminsdk-pkt20-eb15ce9f27.json"
