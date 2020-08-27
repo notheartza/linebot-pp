bcd0c3986e41bee60f3c000fd7c07793c09416a8
notheartza <not.102@hotmail.com>
2020-08-27 15:22:49 +0700

    test

--- linebot-pp/app.py
+++ linebot-pp/app.py
@@ -170,8 +170,16 @@ def realtimebase():
     userresults = client.open("linebothistory").get_worksheet(0)
     userssheet = userresults.get_all_records()
     for i in userssheet:
-        print(i)
-        firebase_rdb.child("users").push(i)
+        data = {
+            "date": i["date"],
+            "userName": i["userName"],
+            "pictureProfile": i["pictureProfile"],
+            "status": i["สถานะ"],
+            "room": i['room'],
+            "number": i['number']
+        }
+
+        firebase_rdb.child("users").child(i["userId"]).push(data)
 
     return 'finish'
 
