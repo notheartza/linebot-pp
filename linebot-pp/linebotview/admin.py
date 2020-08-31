8f7a2f1a432dfa24350a299a8ac05694432a023d
notheartza <not.102@hotmail.com>
2020-08-31 15:10:07 +0700

    t

--- linebot-pp/linebotview/admin.py
+++ linebot-pp/linebotview/admin.py
@@ -55,7 +55,7 @@ def page_admin():
         chat_rdb = firebase_rdb.child('users').child(select).get()
         print(chat_rdb.val())
         for i in chat_rdb.each():
-              print(i.val()['chat'])
+              print(i.val()[0])
 
     else:
         chat_rdb = []
