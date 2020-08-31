0c2c88e198b2445585c633ab93ab04dc222fb916
notheartza <not.102@hotmail.com>
2020-08-31 20:56:44 +0700

    t

--- linebot-pp/linebotview/admin.py
+++ linebot-pp/linebotview/admin.py
@@ -57,7 +57,7 @@ def page_admin():
         test = chat_rdb.val()
         getjson = json.dumps(test['chat'])
         getjson = json.loads(getjson)
-        print(getjson['621'])
+        print(getjson)
     else:
         test = []
     
