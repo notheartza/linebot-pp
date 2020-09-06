a012adee404c4ccf4df43984f33af23da8122ab0
notheartza <not.102@hotmail.com>
2020-09-06 23:28:08 +0700

    t

--- linebot-pp/exam/exam.py
+++ linebot-pp/exam/exam.py
@@ -1,40 +1,23 @@
-from flask import Flask, request, abort, render_template, url_for, make_response, redirect, Blueprint
-from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth, MultiAuth
+from flask import Flask, request, abort, render_template, url_for, make_response, redirect, Blueprint, session, flash
 from ..firebase.config_firebase import firebase_db, firebase_rdb
-from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import TimedJSONWebSignatureSerializer as JWS
+from functools import wraps
+import jwt
 import json
 
 
 exam_page = Blueprint('exam_page', __name__)
-exam_auth = HTTPBasicAuth()
-token_auth = HTTPTokenAuth('Exam')
-multi_auth = MultiAuth(exam_auth, token_auth)
-jws = JWS('exam_pp', expires_in=3600)
 
 
-@exam_auth.verify_password
-def verify_password(username, password):
-    print("checking....")
-    if firebase_rdb.child('exam').child('user').child(username).get().val() is not None:
-        #res = make_response(redirect('/exam'))
-        #res.set_cookie('username', value='Test', max_age=3600)
-        return username
-
-
-@token_auth.verify_token
-def verify_token(token):
-    try:
-        data = jws.loads(token)
-    except:  # noqa: E722
-        return False
-    if 'username' in data:
-        return data['username']
-
-
-@exam_page.route('/exam')
-@multi_auth.login_required
+@exam_page.route('/exam', methods=['POST'])
 def exam():
-    id = multi_auth.current_user()
-    user = firebase_rdb.child('exam').child('user').child(id).get().val()
-    return render_template('exam.html', user=user)
+    if request.form.get('token') is None:
+        if None not in (request.form['username'], request.form['password']):
+            user = request.form['username'] 
+            password = request.form['password']
+            token =jwt.encode({'user': user, 'password': password}, 'secret', algorithm='HS256')
+            redirect(url_for(f'/exam?token={token}'))
+        else:
+            return render_template('login.html')
+    else:  
+        #user = firebase_rdb.child('exam').child('user').child(id).get().val()
+        return render_template('exam.html', user=user)
