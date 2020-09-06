2520d87a075fea15f53fa41e80319c7ee17bb800
notheartza <not.102@hotmail.com>
2020-09-06 23:42:07 +0700

    t

--- linebot-pp/exam/exam.py
+++ linebot-pp/exam/exam.py
@@ -11,13 +11,14 @@ exam_page = Blueprint('exam_page', __name__)
 @exam_page.route('/exam', methods=['GET', 'POST'])
 def exam():
     if request.form.get('token') is None:
-        if None not in (request.form['username'], request.form['password']):
-            user = request.form['username'] 
-            password = request.form['password']
-            token =jwt.encode({'user': user, 'password': password}, 'secret', algorithm='HS256')
-            redirect(url_for(f'/exam?token={token}'))
-        else:
-            return render_template('login.html')
+        if request.method == "POST":
+            if None not in (request.form['username'], request.form['password']):
+                user = request.form['username'] 
+                password = request.form['password']
+                token =jwt.encode({'user': user, 'password': password}, 'secret', algorithm='HS256')
+                redirect(url_for(f'/exam?token={token}'))
+            else:
+                return render_template('login.html')
     else:  
         #user = firebase_rdb.child('exam').child('user').child(id).get().val()
         return render_template('exam.html', user=user)
