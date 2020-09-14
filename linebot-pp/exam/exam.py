b4dd6679b288b0f70703bfbf6708124ebd4432d6
notheartza <not.102@hotmail.com>
2020-09-14 15:06:29 +0700

    t

--- linebot-pp/exam/exam.py
+++ linebot-pp/exam/exam.py
@@ -18,8 +18,9 @@ def exam():
         return redirect(f"/exam/login")
     else:
         if request.method == "POST":
-
-
+            get_choice = request.form["optradio"]
+            header = request.form["header"]
+            print(f"{get_choice} : {header}")
             pass
         else:
             token = request.args.get("token")
